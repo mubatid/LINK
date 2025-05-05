ce93d001ee61c0e76563766725caf8ab1cbe04f6
mubatid <mubatasu@gmail.com>
2025-05-05 15:53:02 +0700

    Update links.js

--- pages/links.js
+++ pages/links.js
@@ -1,5 +1,5 @@
 const shortlinks = {
   "kaos88": "https://s.shopee.co.id/2fyLjOkN8F",
-  "promo1": "https://tokopedia.link/promo-menarik",
+  "promo1": "https://s.shopee.co.id/9zkwXh2Vlc",
   "yt": "https://youtube.com/"
 };
